aa5d753c095a32f0f370992482af42060b305939
Anushan <anushan559@gmail.com>
2024-03-12 17:43:59 +0530

    compment basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from  "react"
+import Button from "./Button"
+import "./style.css"
+const App=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>HELLO ANUSHAN</h1>
+            <Button title="PlatStore"></Button>
+            <Button title="Appstore"></Button>
+        </div>
+    )
 }
 
 export default App;
